68759844372e196d8980976e02665190ecbbb6d4
EZIANDREAN <tobingirawan055@gmail.com>
2025-06-23 12:14:12 +0800

    Update Soal1.py

--- modul 2/tugas-modul-2/KODINGAN/Soal1.py
+++ modul 2/tugas-modul-2/KODINGAN/Soal1.py
@@ -1,41 +1,44 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.prev = None
-        self.next = None
+class Node: #untuk membuat struktur dasar dari sebuah simpul dalam node
+    def __init__(self, data): #ntuk mengatur data awal saat node baru 
+        dibuat
+        
+        self.data = data #untuk menyimpan nilai yang dimasukan dalam node
+        self.prev = None #self prev adalah reverensi  ke node  sebelumnya
+        self.next = None # self next adalah reverensi ke node berikutnya
 
-class DoubleLinkedList:
-    def __init__(self):
-        self.head = None
+class DoubleLinkedList: #membuat class doble linklst yaitu kumpulan node
+    yang saling terhubung dua arah
+    def __init__(self): #metode khusus untuk inisialisasi ketika objek linklist dibuat
+        self.head = None #head adalah simpul pertama atau kepala dari linklist
 
-    def tambah_di_akhir(self, data):
-        new_node = Node(data)
-        if self.head is None:
-            self.head = new_node
-            return
-        curr = self.head
-        while curr.next:
-            curr = curr.next
-        curr.next = new_node
-        new_node.prev = curr
+    def tambah_di_akhir(self, data): #untuk menambahkan node baru di akhir linklist
+        new_node = Node(data) #membuat node baru berisi data yang ingin dimasukan 
+        if self.head is None: #untuk cek jika list masih kosong
+            self.head = new_node # maka node baru dibuat untuk menjadi kepala
+            return #untuk menghentikan proses karna node sudah ditambahkan
+        curr = self.head #jika list tidak kosong, mulai dari node kepala
+        while curr.next: #selama masih ada node berikutnya maka lanjut ke node berikutnya
+            curr = curr.next  #hingga sampai ke node terakhir
+        curr.next = new_node #untuk menghubungkan node terakhir
+        new_node.prev = curr #untuk menghubungkan node baru ke node sebelumnya
 
-    def hapus_node_awal(self):
-        if self.head is None:
-            print("Linked list kosong. Tidak ada yang bisa dihapus.")
-            return
-        print(f"Menghapus node dengan data: {self.head.data}")
-        if self.head.next is None:
+    def hapus_node_awal(self): #untuk menghapus node pertama (head) dari linklist
+        if self.head is None: #jika list kosong maka tidak ada yang bisa dihapus
+            print("Linked list kosong. Tidak ada yang bisa dihapus.") #untuk menampilkan pesan bahwa list kosong
+            return #menghentikan fungsi
+        print(f"Menghapus node dengan data: {self.head.data}") #untuk menampilkan data node yang akan dihapus
+        if self.head.next is None: #untuk cek jika hanya ada satu node yang dihapus
             self.head = None  # List jadi kosong
         else:
-            self.head = self.head.next
-            self.head.prev = None
+            self.head = self.head.next #untuk menggeser head ke node berikutnya
+            self.head.prev = None #untuk menghapus hubungan balik ke node lama
 
-    def tampilkan(self):
-        curr = self.head
-        while curr:
-            print(curr.data, end=" <-> ")
-            curr = curr.next
-        print("None")
+    def tampilkan(self): #fungsi untuk menampilkan semua data di list
+        curr = self.head #untuk memulai list dari kepala
+        while curr: #selama node masih ada
+            print(curr.data, end=" <-> ") #untuk menampilkan data dengan tanda panah dua arah
+            curr = curr.next #untuk lanjut ke node berikutnya
+        print("None") #untuk menampilkan list terakhir
 
 # Contoh penggunaan
 dll = DoubleLinkedList()
